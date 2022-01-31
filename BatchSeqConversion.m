9e15b15032afa2a4a0ccaa4af3e8ca2e0f83ba1e
rustynerve <hsingchien.zhang@gmail.com>
2022-01-30 16:51:57 -0800

    no message

--- BatchSeqConversion.m
+++ BatchSeqConversion.m
@@ -1,7 +1,7 @@
 behav  = dir('**\BehavCam_0');
 behav = unique({behav.folder});
 to_remove = [];
-for i = 1:length(behav)
+for i = 11:length(behav)
 if contains(behav{i},'sep')
 to_remove = [to_remove, i];
 end
@@ -9,15 +9,17 @@ end
 behav(to_remove) = [];
 for i = 1:length(behav)
 cd(behav{i});
-if ~exist('behav_video.avi')
+if exist('0.avi')
 VideoCombine(pwd, 'b',1, true, 'avi');
 end
-if isempty(dir('*.seq'))
+if isempty(dir('*.seq')) & exist('behav_video.avi')
 seqName = 'behavior.seq';
 aviName = 'behav_video.avi';
 seqIo([seqName],'frImgs',struct('codec','png'),'aviName',[aviName]);
 end
-movefile behav_video.avi behav_video f;
-delete *.avi;
-movefile behav_video behav_video.avi f;
+try
+    movefile behav_video.avi behav_video f;
+    delete *.avi;
+    movefile behav_video behav_video.avi f;
+end
 end
